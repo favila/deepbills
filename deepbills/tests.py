c3f1d7c9f3cd199a0bd9e22c5921d664600ff35a
Francis Avila <favila@dancingmammoth.com>
2012-09-21 13:51:03 -0500

    Unittests for queries
    Adds QueryTests class and test_utf8_document_xquery_add() test case to tickle bug with binding a non-ascii string to a query parameter. Always bind unicode values to query parameters!

--- deepbills/tests.py
+++ deepbills/tests.py
@@ -1,16 +1,37 @@
+# coding: utf-8
 import unittest
-
 from pyramid import testing
 
-class ViewTests(unittest.TestCase):
+class QueryTests(unittest.TestCase):
+    testdbname = 'testdb'
     def setUp(self):
-        self.config = testing.setUp()
+        from .views import sessionfactory
+        self.session = sessionfactory()
+        self.session.defaultDB = self.testdbname 
+        self.session.execute('SET MAINMEM true')
+        self.session.execute('CREATE DB %s' % self.testdbname)
+        self.session.execute('SET MAINMEM false')
 
-    def tearDown(self):
-        testing.tearDown()
+    def TearDown(self):
+        self.session.execute('DROP DB %s' % self.testdbname)
+        self.session.close()
 
-    def test_my_view(self):
-        from .views import my_view
-        request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['project'], 'deepbills')
+    def test_utf8_document_xquery_add(self):
+        sess = self.session
+        specialchardoc = u'<root>\u2014</root>'
+        specialchardoc_utf8 = specialchardoc.encode('utf-8')
+        resourcepath = 'QueryTests/test_utf8_document_xquery_add.xml'
+        
+        xqadd = ("db:add($DB, $data, $path)", ['data','path'])
+        with sess.query(*xqadd) as q:
+            q.bind('data', specialchardoc)
+            q.bind('path', resourcepath)
+            q.execute()
+        
+        xqverify = ("db:open($DB, $path)", ['path'])
+        with sess.query(*xqverify) as q:
+            q.bind('path', resourcepath)
+            result = q.execute()
+        self.assertEqual(specialchardoc, result)
+
+
