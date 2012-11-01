9dab75f991f263e66b6dce0380ba6ec853c75bb2
Francis Avila <favila@dancingmammoth.com>
2012-11-01 10:50:52 -0500

    Alter editor paths and static routing

--- deepbills/__init__.py
+++ deepbills/__init__.py
@@ -33,13 +33,20 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
-    config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('dashboard', '/')
     config.add_route('query', '/query')
     config.add_route('bill_view', '/bills/{docid}/view')
     config.add_route('bill_edit', '/bills/{docid}/edit')
     config.add_route('bill_resource', '/bills/{docid}')
     config.add_route('vocabulary_lookup', '/vocabularies/{vocabid}')
+
+    config.add_static_view('Editor', 'static/AKN/Editor')
+    config.add_static_view('HouseXML', 'static/AKN/HouseXML')
+    config.add_static_view('BCN', 'static/AKN/BCN')
+    config.add_static_view('AkomaNtoso', 'static/AKN/AkomaNtoso')
+
+    config.add_route('akn_editor', 'Editor/Index.html')
+    config.add_static_view('static', 'static', cache_max_age=3600)
 
     config.scan()
     return config.make_wsgi_app()
