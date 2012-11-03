401a11791401de50b140d8fb608b1e8ff519f592
Francis Avila <favila@dancingmammoth.com>
2012-11-02 19:32:00 -0500

    Remove static routes no longer needed for post-0.28 editor

--- deepbills/__init__.py
+++ deepbills/__init__.py
@@ -40,13 +40,11 @@ def main(global_config, **settings):
     config.add_route('bill_resource', '/bills/{docid}')
     config.add_route('vocabulary_lookup', '/vocabularies/{vocabid}')
 
-    config.add_static_view('Editor', 'static/AKN/Editor')
-    config.add_static_view('HouseXML', 'static/AKN/HouseXML')
-    config.add_static_view('BCN', 'static/AKN/BCN')
-    config.add_static_view('AkomaNtoso', 'static/AKN/AkomaNtoso')
 
+    config.add_static_view('Editor', 'static/AKN/Editor')
     config.add_route('akn_editor', 'Editor/Index.html')
     config.add_static_view('static', 'static', cache_max_age=3600)
+
 
     config.scan()
     return config.make_wsgi_app()
