f5419b2df6713a75201ff4a7d04c5db501d4b301
Francis Avila <favila@dancingmammoth.com>
2012-11-06 02:34:41 -0600

    Fix utf8 document unittest (using a temporary db) because of changed basexclient interface

--- deepbills/tests.py
+++ deepbills/tests.py
@@ -1,13 +1,13 @@
 # coding: utf-8
 import unittest
 from pyramid import testing
+from pyramid.httpexceptions import HTTPNotFound
 
 class QueryTests(unittest.TestCase):
     testdbname = 'testdb'
     def setUp(self):
         from .views import sessionfactory
         self.session = sessionfactory()
-        self.session.defaultDB = self.testdbname 
         self.session.execute('SET MAINMEM true')
         self.session.execute('CREATE DB %s' % self.testdbname)
         self.session.execute('SET MAINMEM false')
@@ -22,14 +22,16 @@ class QueryTests(unittest.TestCase):
         specialchardoc_utf8 = specialchardoc.encode('utf-8')
         resourcepath = 'QueryTests/test_utf8_document_xquery_add.xml'
         
-        xqadd = ("db:add($DB, $data, $path)", ['data','path'])
+        xqadd = ("db:add($testdb, $data, $path)", ['testdb','data','path'])
         with sess.query(*xqadd) as q:
+            q.bind('testdb', self.testdbname)
             q.bind('data', specialchardoc)
             q.bind('path', resourcepath)
             q.execute()
         
-        xqverify = ("db:open($DB, $path)", ['path'])
+        xqverify = ("db:open($testdb, $path)", ['testdb', 'path'])
         with sess.query(*xqverify) as q:
+            q.bind('testdb', self.testdbname)
             q.bind('path', resourcepath)
             result = q.execute()
         self.assertEqual(specialchardoc, result)
